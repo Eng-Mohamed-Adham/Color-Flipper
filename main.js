236551e792b683eafad09a82def3585fb77e6860
Eng-mohamed-Adham <pythonh37@gmail.com>
2021-12-31 18:15:41 +0200

    this is files app

---
+++ main.js
@@ -0,0 +1,20 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    // get random number between 0 _ 3 
+
+    const randomNumber = getRandomNumder();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colors[randomNumber];
+
+    color.textContent = colors[randomNumber];
+});
+
+
+function getRandomNumder(){
+    return Math.floor(Math.random() * colors.length);
+}
